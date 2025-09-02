abaa877ce7fced818f54e29b3c5db93b2ace64cb
RomainGuillaume120503 <romain.guillaume12@gmail.com>
2025-09-03 00:21:53 +0200

    2e commit avec le baseApi

---
+++ CasinoFront/src/api/baseApi.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import TokenService from '@/services/token.service';
+import router from '@/routers';
+
+const api = axios.create({
+  baseURL: 'https://localhost:7157/api/v1/',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = TokenService.getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const status = error.response?.status;
+
+    if (status === 401) {
+      TokenService.removeToken();
+      router.push({ name: 'Login' });
+    }
+
+    if (status === 403) {
+      router.push({ name: 'Forbidden' }); // Ta page d'erreur 403
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
